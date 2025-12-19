1e6c40e5164c18c9a7fa675c66bb864f47e3c032
Tetiana <dak7214@gmail.com>
2025-12-19 23:29:02 +0200

    add update files

--- src/server.js
+++ src/server.js
@@ -1,18 +1,17 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
-import cors from "cors";
-import { errors } from "celebrate";
+import cookieParser from 'cookie-parser';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRoutes from './routes/notesRoutes.js';
-// import pinoHttp from "pino-http";
+import authRoutes from './routes/authRoutes.js';
+
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
-
-
-
+import { errors } from 'celebrate';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -20,19 +19,22 @@ const PORT = process.env.PORT ?? 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
-// app.use(pinoHttp());
+app.use(cookieParser());
 
-
+/// Маршрути
 app.use(notesRoutes);
-app.use(errors());
-app.use(notFoundHandler);
-app.use(errorHandler);
+app.use(authRoutes);
 
+// Middleware 404
+app.use(notFoundHandler);
+
+app.use(errors());
+
+// Middleware для обробки помилок
+app.use(errorHandler);
 
 await connectMongoDB();
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
-
